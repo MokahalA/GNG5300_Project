122e21a7190a293d54f51ead8d6dafb4c97b8d51
s-cui801 <scui059@uOttawa.ca>
2024-11-19 20:35:22 -0500

    Create models for groceries; implement script populate_categories.py to initialize categories.

--- mealgenie/users/admin.py
+++ mealgenie/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from users.models import UserProfile, GroceryCategory, Grocery, UserGrocery
 
 # Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'dietary_preferences', 'allergies']
+
+class GroceryCategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_name']
+
+class GroceryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category', 'unit']
+
+class UserGroceryAdmin(admin.ModelAdmin):
+    list_display = ['user', 'grocery', 'quantity', 'expiration_date']
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(GroceryCategory, GroceryCategoryAdmin)
+admin.site.register(Grocery, GroceryAdmin)
+admin.site.register(UserGrocery, UserGroceryAdmin)
--- mealgenie/users/models.py
+++ mealgenie/users/models.py
@@ -6,3 +6,21 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     dietary_preferences = models.TextField(blank=True)
     allergies = models.TextField(blank=True)
+
+class GroceryCategory(models.Model):
+    category_name = models.CharField(max_length=100)
+
+class Grocery(models.Model):
+    grocery_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(GroceryCategory, on_delete=models.CASCADE)
+    unit = models.CharField(max_length=100)
+    # image = models.ImageField(upload_to='grocery_images', blank=True)
+
+
+class UserGrocery(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    grocery = models.ForeignKey(Grocery, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    expiration_date = models.DateField(blank=True)
