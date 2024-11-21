72fb2768a686f2c94323252132f594606011c122
s-cui801 <scui059@uOttawa.ca>
2024-11-20 23:01:57 -0500

    update database schema

--- mealgenie/users/models.py
+++ mealgenie/users/models.py
@@ -10,13 +10,34 @@ class UserProfile(models.Model):
 class GroceryCategory(models.Model):
     category_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.category_name.capitalize()
+
 class Grocery(models.Model):
+    # Define choices as class constants
+    LB = 'lb'
+    KG = 'kg'
+    UNIT = 'unit(s)'
+    
+    UNIT_CHOICES = [
+        (LB, 'Pound'),
+        (KG, 'Kilogram'),
+        (UNIT, 'Unit(s)'),
+    ]
+
     grocery_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
     category = models.ForeignKey(GroceryCategory, on_delete=models.CASCADE)
-    unit = models.CharField(max_length=100)
+    unit = models.CharField(
+        max_length=10,
+        choices=UNIT_CHOICES,
+        default=UNIT
+    )
     # image = models.ImageField(upload_to='grocery_images', blank=True)
+
+    def __str__(self):
+        return f"{self.name.capitalize()} ({self.category.category_name.capitalize()})"
 
 
 class UserGrocery(models.Model):
