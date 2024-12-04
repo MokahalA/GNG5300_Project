48b7744da347c0a32551f3150da5d7cce11dea9d
s-cui801 <scui059@uOttawa.ca>
2024-12-04 18:48:37 -0500

    Populate categories only when the table is empty

--- mealgenie/users/scripts/populate_categories.py
+++ mealgenie/users/scripts/populate_categories.py
@@ -2,22 +2,26 @@ from users.models import GroceryCategory
 
 def populate_categories():
     # List of categories to add
-    categories = [
-        "Fruits", "Vegetables", "Dairy", "Meat", "Seafood",
-        "Grains", "Pasta", "Baking Supplies", "Spices",
-        "Beverages", "Snacks", "Frozen Foods", "Cleaning Products",
-        "Toiletries", "Baby Food", "Pet Food", "Health Supplements",
-        "Canned Goods", "Condiments", "Sauces", "Breakfast Cereals",
-        "Nuts and Seeds", "Herbs", "Cooking Oils", "Sweets and Desserts"
-    ]
+    if GroceryCategory.objects.all().exists():
+        print("Categories already exist.")
+        return
+    else:
+        categories = [
+            "Fruits", "Vegetables", "Dairy", "Meat", "Seafood",
+            "Grains", "Pasta", "Baking Supplies", "Spices",
+            "Beverages", "Snacks", "Frozen Foods", "Cleaning Products",
+            "Toiletries", "Baby Food", "Pet Food", "Health Supplements",
+            "Canned Goods", "Condiments", "Sauces", "Breakfast Cereals",
+            "Nuts and Seeds", "Herbs", "Cooking Oils", "Sweets and Desserts"
+        ]
 
-    # Create categories if they don't already exist
-    for category in categories:
-        category, created = GroceryCategory.objects.get_or_create(category_name=category.lower())
-        if created:
-            print(f"Added category: {category.category_name.capitalize()}")
-        else:
-            print(f"Category already exists: {category.category_name.capitalize()}")
+        # Create categories if they don't already exist
+        for category in categories:
+            category, created = GroceryCategory.objects.get_or_create(category_name=category.lower())
+            if created:
+                print(f"Added category: {category.category_name.capitalize()}")
+            else:
+                print(f"Category already exists: {category.category_name.capitalize()}")
 
 if __name__ == "__main__":
     populate_categories()
